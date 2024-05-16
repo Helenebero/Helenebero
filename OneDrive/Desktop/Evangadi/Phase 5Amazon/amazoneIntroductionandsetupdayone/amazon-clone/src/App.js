bbea5e3118418ead1a1092a233da302f770514f2
Helenebero <156723506+Helenebero@users.noreply.github.com>
2024-05-16 10:50:09 -0500

    header

---
+++ OneDrive/Desktop/Evangadi/Phase 5Amazon/amazoneIntroductionandsetupdayone/amazon-clone/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Header from "./Components/Header/Header";
+import Test1 from "./Test/Test1";
+import Test2 from "./Test/Test2";
+function App() {
+  return (
+    <div >
+      <Header />
+      {/* <Test1 />
+      <br />
+      <Test2 /> */}
+    </div>
+  );
+}
+
+export default App;
