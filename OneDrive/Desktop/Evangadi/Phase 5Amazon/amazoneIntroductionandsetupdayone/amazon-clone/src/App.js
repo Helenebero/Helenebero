faaa6c9a3b49adb3bdc8ad026cb2531397cad446
Helenebero <156723506+Helenebero@users.noreply.github.com>
2024-05-16 22:22:00 -0500

    header

--- OneDrive/Desktop/Evangadi/Phase 5Amazon/amazoneIntroductionandsetupdayone/amazon-clone/src/App.js
+++ OneDrive/Desktop/Evangadi/Phase 5Amazon/amazoneIntroductionandsetupdayone/amazon-clone/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
-import './App.css';
-import Header from "./Components/Header/Header";
-import Test1 from "./Test/Test1";
-import Test2 from "./Test/Test2";
+//import './App.css';
+//import Test1 from "./Test/Test1";
+//import Test2 from "./Test/Test2";
+import Header from "./Components/Header/Header"
+import Carousel from "./Components/Carousel/CarouselEffect";
+
+
 function App() {
   return (
     <div >
       <Header />
+      <Carousel />
       {/* <Test1 />
       <br />
       <Test2 /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
